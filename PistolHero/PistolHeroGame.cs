11cf04f7a5c7d0cbfb7c3d1147cd02ccab500f75
Minipeps <mpcaudan@gmail.com>
2020-03-08 17:14:39 +0100

    Initial commit

--- PistolHero/PistolHeroGame.cs
+++ PistolHero/PistolHeroGame.cs
@@ -7,12 +7,12 @@ namespace PistolHero
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class PistolHeroGame : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         
-        public Game1()
+        public PistolHeroGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- PistolHero/Program.cs
+++ PistolHero/Program.cs
@@ -13,7 +13,7 @@ namespace PistolHero
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new PistolHeroGame())
                 game.Run();
         }
     }
